63562a8f73695ac9cb890da33d89fcc8e53c6923
alezxandro <gheojan.alessandro@outlook.com>
2023-11-14 09:03:50 +0100

    declaring methods for dynamic menu

--- src/DynamicMethod.java
+++ src/DynamicMethod.java
@@ -1,3 +1,19 @@
 public class DynamicMethod {
-    
+
+    @MenuOption(name = "Option 1")
+    public void method1() {
+        System.out.println("DynamicMethod.method1()");
+    }
+    @MenuOption(name = "Option 2")
+    public void method2() {
+        System.out.println("DynamicMethod.method2()");
+    }
+    @MenuOption(name = "Option 3")
+    public void method3() {
+        System.out.println("DynamicMethod.method3()");
+    }
+    @MenuOption(name = "Option 4")
+    public void method4() {
+        System.out.println("DynamicMethod.method4()");
+    }
 }
